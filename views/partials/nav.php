b125616d174999a71ea34ebc024ba4ff0cd93fa7
sytnic <sytnic@list.ru>
2023-05-04 11:46:49 +0300

    14. Ternary operator

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,13 +8,13 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/"            class="<?php if($_SERVER['REQUEST_URI'] == "/") {echo "bg-gray-900 text-white ";} else {echo "text-gray-300 ";}  ?>              
+              <a href="/"            class="<?php echo $_SERVER['REQUEST_URI'] == "/" ? "bg-gray-900 text-white " : "text-gray-300 "; ?>              
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
               
-              <a href="/about.php"   class="<?php if($_SERVER['REQUEST_URI'] == "/about.php") {echo "bg-gray-900 text-white ";} else {echo "text-gray-300 ";}  ?>
+              <a href="/about.php"   class="<?php echo $_SERVER['REQUEST_URI'] == "/about.php" ? "bg-gray-900 text-white " : "text-gray-300 "; ?>
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">About</a>
               
-              <a href="/contact.php" class="<?php if($_SERVER['REQUEST_URI'] == "/contact.php") {echo "bg-gray-900 text-white ";} else {echo "text-gray-300 ";}  ?>
+              <a href="/contact.php" class="<?= $_SERVER['REQUEST_URI'] == "/contact.php" ? "bg-gray-900 text-white " : "text-gray-300 " ?>
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">Contact</a>
               
               <a href="#"            class="text-gray-300 hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">Testing</a>
