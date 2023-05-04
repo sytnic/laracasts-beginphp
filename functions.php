312581efe8df4465a89f5bcb8e070460e3596939
sytnic <sytnic@list.ru>
2023-05-04 14:21:59 +0300

    14. Require functions

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// dump and die
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+//dd($_SERVER);
+
+/**
+ * Used in /views/partials for styling of navbar
+ * 
+ * @return bool
+ */
+function urlIs($value) {
+    return $_SERVER['REQUEST_URI'] == $value;
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 
+require 'functions.php';
+
 $heading = "Home";
-
-// dump and die
-function dd($value) {
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
-
-//dd($_SERVER);
 
 require "views/index.view.php";
 
