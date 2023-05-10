e0b97d0d479a90b960dbf7425a6a1ab223ec0557
sytnic <sytnic@list.ru>
2023-05-10 17:16:10 +0300

    15.4 Page 404

--- index.php
+++ index.php
@@ -11,15 +11,29 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 //$uri_array = parse_url($uri_request);
 //$uri = $uri_array['path'];
 
-
+// маршруты
 $routes = [
     '/'        => 'controllers/index.php',
     '/about'   => 'controllers/about.php',
     '/contact' => 'controllers/contact.php'
 ];
 
+// направление
 if (array_key_exists($uri, $routes)) {
+    // если ключ есть в роутах, задействуй его
     require $routes[$uri];
+
+} else {  // иначе
+    http_response_code(404);
+
+    // Вызов своего сообщения для ошибки 404
+    // echo "Sorry. Not Found.";
+
+    // Вызов своей страницы для ошибки 404
+    require 'views/404.php';
+
+    // остановка загрузки страницы
+    die();
 }
 
 
