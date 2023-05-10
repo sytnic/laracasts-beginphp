02b0a2787b7302335207ce65177621312cbebfd6
sytnic <sytnic@list.ru>
2023-05-10 16:47:20 +0300

    15.3 Routes

--- index.php
+++ index.php
@@ -2,25 +2,24 @@
 
 require 'functions.php';
 
-// парсинг url-строки с выдергиванием главного пути без параметров запроса
-//$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-//dd($uri);
+// парсинг url-строки с выдергиванием 
+// главного пути без параметров запроса
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 // or:
-$uri_request = $_SERVER['REQUEST_URI'];
-$uri_array = parse_url($uri_request);
-$uri = $uri_array['path'];
+//$uri_request = $_SERVER['REQUEST_URI'];
+//$uri_array = parse_url($uri_request);
+//$uri = $uri_array['path'];
 
-// @return array
-//dd(parse_url($uri));
 
-//echo $uri;
+$routes = [
+    '/'        => 'controllers/index.php',
+    '/about'   => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php'
+];
 
-if ($uri == '/') {
-    require 'controllers/index.php';
-} elseif ($uri == '/about') {
-    require 'controllers/about.php';
-} elseif ($uri == '/contact') {
-    require 'controllers/contact.php';
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
 }
 
 
