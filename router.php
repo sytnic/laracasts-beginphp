d307e06128f38ab6f212e503f609b7759e0729ce
sytnic <sytnic@list.ru>
2023-05-11 15:15:38 +0300

    15.7 Router.php

---
+++ router.php
@@ -0,0 +1,56 @@
+<?php
+
+// парсинг url-строки с выдергиванием 
+// главного пути без параметров запроса
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+// or:
+//$uri_request = $_SERVER['REQUEST_URI'];
+//$uri_array = parse_url($uri_request);
+//$uri = $uri_array['path'];
+
+// маршруты
+$routes = [
+    '/'        => 'controllers/index.php',
+    '/about'   => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php'
+];
+
+/**
+ * Показ своей страницы ошибки (по умолч. 404)
+ * 
+ * @param int - по умолчанию 404
+ * 
+ */
+function abort($code = 404) {
+    http_response_code($code);
+
+    // Вызов своей страницы для ошибки 404
+    require "views/{$code}.php";
+
+    // остановка загрузки страницы
+    die();
+}
+
+/**
+ * Вызывает затребование страницы согласно маршруту
+ * или 404
+ * 
+ * @param string $uri
+ * @param array  $routes
+ * 
+ */
+function routeToController($uri, $routes) {
+    // направление
+    if (array_key_exists($uri, $routes)) {
+        // если ключ есть в роутах, задействуй его
+        require $routes[$uri];
+    } else {  // иначе
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
+
+
+?>
