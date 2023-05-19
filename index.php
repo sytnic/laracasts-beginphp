de46a118e72d9c632f14c2532b78b59bb0c2f82f
sytnic <sytnic@list.ru>
2023-05-19 16:46:35 +0300

    17.1 PDO. Fetch Assoc.

--- index.php
+++ index.php
@@ -6,16 +6,23 @@ require 'functions.php';
 
 // Получение данных из БД с помощью PDO
 
-// connect to our MySQL database
+// Подключение без указания user в $dsn
+/*
 $dsn = "mysql:host=localhost; port=3306; dbname=myapp; charset=utf8mb4";
-
 $pdo = new PDO($dsn, 'root');
+*/
+
+// Подключение с указанием user в $dsn
+// Требуется указать и пароль, если он есть
+$dsn = "mysql:host=localhost; port=3306; dbname=myapp; user=root; charset=utf8mb4";
+$pdo = new PDO($dsn);
+
 
 $statement = $pdo->prepare("select * from posts");
 
 $statement->execute();
 
-$posts = $statement->fetchAll();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
 
