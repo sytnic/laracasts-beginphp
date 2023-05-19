1b6e44c44b098c995fe1d5c0d7b932413f076346
sytnic <sytnic@list.ru>
2023-05-19 16:33:41 +0300

    17.0 PDO. Simple connect. Fetch All.

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
 
+
+// Получение данных из БД с помощью PDO
+
+// connect to our MySQL database
+$dsn = "mysql:host=localhost; port=3306; dbname=myapp; charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
 
 ?>
