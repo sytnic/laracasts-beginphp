fc23a59403cf5032b52b94f1891b239aabd8b3d2
sytnic <sytnic@list.ru>
2023-05-22 11:38:35 +0300

    18.0 PDO in class

--- index.php
+++ index.php
@@ -3,28 +3,22 @@
 require 'functions.php';
 //require 'router.php';
 
+class Database {
+    public function query(){
+        $dsn = "mysql:host=localhost; port=3306; dbname=myapp; user=root; charset=utf8mb4";
+        $pdo = new PDO($dsn);
+                
+        $statement = $pdo->prepare("select * from posts");
+        
+        $statement->execute();
+        
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
 
-// Получение данных из БД с помощью PDO
+$db = new Database;
 
-// Подключение без указания user в $dsn
-/*
-$dsn = "mysql:host=localhost; port=3306; dbname=myapp; charset=utf8mb4";
-$pdo = new PDO($dsn, 'root');
-*/
-
-// Подключение с указанием user в $dsn
-// Требуется указать и пароль, если он есть
-$dsn = "mysql:host=localhost; port=3306; dbname=myapp; user=root; charset=utf8mb4";
-$pdo = new PDO($dsn);
-
-
-$statement = $pdo->prepare("select * from posts");
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-//dd($posts);
+$posts = $db->query();
 
 foreach ($posts as $post) {
     echo "<li>".$post['title']."</li>";
