032249157e1cb60dc7b93e6b23bfe3b71f03432d
sytnic <sytnic@list.ru>
2023-05-23 14:48:22 +0300

    18.1 Universal query. Constructor in Database Class.

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ require 'functions.php';
 //require 'router.php';
 
 class Database {
-    public function query(){
+
+    public $connection;
+
+    public function __construct()
+    {
         $dsn = "mysql:host=localhost; port=3306; dbname=myapp; user=root; charset=utf8mb4";
-        $pdo = new PDO($dsn);
-                
-        $statement = $pdo->prepare("select * from posts");
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
         
         $statement->execute();
         
@@ -18,7 +25,8 @@ class Database {
 
 $db = new Database;
 
-$posts = $db->query();
+$posts = $db->query("select * from posts");
+// $posts = $db->query("select * from posts where id > 1");
 
 foreach ($posts as $post) {
     echo "<li>".$post['title']."</li>";
