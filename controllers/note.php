65e2c86bc136d3b9f1ed716cba08ee7739017eb9
sytnic <sytnic@list.ru>
2023-06-08 17:06:54 +0300

    23.0 Forbidding access to a note.

--- controllers/note.php
+++ controllers/note.php
@@ -5,10 +5,14 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
-$id = $_GET['id'];
+$note = $db->query('select * from notes where user_id = :user and id = :id', [
+    'user' => 1,
+    'id'   => $_GET['id']
+])->fetch();
 
-$note = $db->query('select * from notes where id = :id', ['id' => $id])->fetch();
-
+if(!$note){
+    abort();
+}
 
 require "views/note.view.php";
 
