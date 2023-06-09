630d9b6f18a2c1007e62602908a4d3d4266b1295
sytnic <sytnic@list.ru>
2023-06-09 16:54:59 +0300

    23.1 Pages 403 and 404.

--- controllers/note.php
+++ controllers/note.php
@@ -5,13 +5,16 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
-$note = $db->query('select * from notes where user_id = :user and id = :id', [
-    'user' => 1,
+$note = $db->query('select * from notes where id = :id', [
     'id'   => $_GET['id']
 ])->fetch();
 
 if(!$note){
     abort();
+}
+
+if($note['user_id'] != 1){
+    abort(403);
 }
 
 require "views/note.view.php";
