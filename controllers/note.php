f48b90c5bf375ac06a9ca813ee76469ffe0f5b61
sytnic <sytnic@list.ru>
2023-09-12 11:16:37 +0300

    24.2 Function authorize()

--- controllers/note.php
+++ controllers/note.php
@@ -12,11 +12,14 @@ $note = $db->query('select * from notes where id = :id', [
 
 $currentUserId = 1;
 
-// если user_id из строки табицы по SQL-запросу не равен 1 - 403
+authorize($note['user_id'] == $currentUserId);
+
+// если user_id из строки таблицы по SQL-запросу не равен 1 - 403
+/*
 if($note['user_id'] != $currentUserId){
     abort(Response::FORBIDDEN);
 }
-
+*/
 require "views/note.view.php";
 
 ?>
--- functions.php
+++ functions.php
@@ -19,5 +19,11 @@ function urlIs($value) {
     return $_SERVER['REQUEST_URI'] == $value;
 }
 
+function authorize($condition, $status = Response::FORBIDDEN) {
+    if (!$condition) {
+        abort($status);
+    }
+}
+
 
 ?>
