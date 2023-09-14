b93d46bf8c36e654e94befd3987c9577b5f0caf7
sytnic <sytnic@list.ru>
2023-09-14 15:17:56 +0300

    27.0 Required in form. Dropdown is hidden

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -1,11 +1,11 @@
-<?php require('partials/head.php');   ?>  
+<?php require('partials/head.php');   ?>
 <?php require('partials/nav.php');    ?>
 <?php require('partials/banner.php'); ?>
 
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 
-      <!--
+    <!--
         This example requires some changes to your config:
         
         ```
@@ -19,33 +19,40 @@
         }
         ```
       -->
-      <form method="POST">
-        <div class="space-y-12">
-          <div class="border-b border-gray-900/10 pb-12">
-            
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              
-              <div class="col-span-full">
-                <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
-                <div class="mt-2">
-                  <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                </div>
-              
+    <form method="POST">
+      <div class="space-y-12">
+        <div class="border-b border-gray-900/10 pb-12">
+
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+            <div class="col-span-full">
+              <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+              <div class="mt-2">
+                <textarea 
+                  id="body" 
+                  name="body" 
+                  rows="3" 
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Here's an idea for a note..."
+                  required
+                ></textarea>
               </div>
-                  
+
             </div>
+
           </div>
-
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
+      </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
         <!--  <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>  -->
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-        </div>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      </div>
 
-      </form>
+    </form>
 
-    </div>
-  </main>
+  </div>
+</main>
 
 <?php require('partials/footer.php');  ?>
