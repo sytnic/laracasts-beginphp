69f78747159b4ce6f927330d1065326ae80225a2
sytnic <sytnic@list.ru>
2023-09-14 15:36:30 +0300

    27.1 Form validation, strlen==0

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,23 @@ $db = new Database($config['database']);
 $heading = "Create Note";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',
-        [ 'body' => $_POST['body'],
-          'user_id' => 1
-        ] );
+    // массив для сообщений об ошибках
+    $errors = [];
+
+    // если длина сообщения равна нулю, то сообщение об ошибке
+    if(strlen($_POST['body']) == 0) {
+      $errors['body'] = 'A body is required';
+    }
+
+    // если ошибок нет, то делаем вставку в БД
+    if(empty($errors)) {
+      $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',
+      [ 'body' => $_POST['body'],
+        'user_id' => 1
+      ] );  
+    }    
+
+    // если ошибки есть, загрузится вью с массивом ошибок
 }
 
 require 'views/note-create.view.php';
