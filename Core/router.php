225f4fb807f5d8a7862e79c55e26362629b87288
sytnic <sytnic@list.ru>
2023-10-17 15:22:02 +0300

    33.1 Method add() in Router

--- Core/router.php
+++ Core/router.php
@@ -9,49 +9,40 @@ class Router {
     // после вызова соответствующего метода
     // будет заполнен массив $this->$routes
 
-    public function get($uri, $controller)
+    public function add($method, $uri, $controller)
     {
         $this->routes[] = [
             'uri' => $uri,
             'controller' => $controller,
-            'method' => 'GET'
-        ];
+            'method' => $method
+        ];  
+        // или как вариант:
+        //$this->routes[] = compact('method','uri','controller'); 
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->add('GET', $uri, $controller);
     }
 
     public function post($uri, $controller)
     {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'POST'
-        ];
+        $this->add('POST', $uri, $controller);
     }
 
     public function delete($uri, $controller)
     {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'DELETE'
-        ];
+        $this->add('DELETE', $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'PATCH'
-        ];
+        $this->add('PATCH', $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => 'PUT'
-        ];
+        $this->add('PUT', $uri, $controller);
     }
 
     public function route($uri, $method)
