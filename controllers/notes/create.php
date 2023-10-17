4d612ba1249aa928594651d8eed85456d4b0d7e2
sytnic <sytnic@list.ru>
2023-10-17 16:26:47 +0300

    34.2 Updated Store and Create

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,46 +1,10 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-require base_path('Core/Validator.php');
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+// Это действие отвечает за отображение формы для создания заметки
 
 $heading = "Create Note";
 // массив для сообщений об ошибках
 $errors = [];
-
-/*
-if (! Validator::email('sdfdsfdsfdsfssdassa')) {
-  dd('that is not a valid email');
-}
-
-// string(14) "email@email.ru"
-dd(Validator::email('email@email.ru'));
-// bool(false)
-dd(Validator::email('sdfdsfdsfdsfssdassa'));
-*/
-
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    // если длина сообщения равна нулю, то сообщение об ошибке
-    // или длина слишком большая
-    if (! Validator::string($_POST['body'], 1, 140))  {
-      $errors['body'] = 'A body of no more than 140 characters is required.';
-    }
-
-    // если ошибок нет, то делаем вставку в БД
-    if(empty($errors)) {
-      $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',
-      [ 'body' => $_POST['body'],
-        'user_id' => 1
-      ] );  
-    }    
-
-    // если ошибки есть, загрузится вью с массивом ошибок
-}
 
 
 view("notes/create.view.php", [
--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -1,3 +1,36 @@
 <?php
 
-dd('HELLO');
+use Core\Validator;
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$errors = [];
+
+// если длина сообщения равна нулю или слишком большая, 
+// то сообщение об ошибке 
+if (! Validator::string($_POST['body'], 1, 140))  {
+    $errors['body'] = 'A body of no more than 140 characters is required.';
+}
+
+// если ошибки есть, загрузится вью с массивом ошибок
+if (!empty($errors)) {
+    return view("notes/create.view.php", [
+        'heading' => $heading,
+        'errors' => $errors
+    ]);
+}
+
+// если ошибок нет, то делаем вставку в БД
+
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',
+[   'body' => $_POST['body'],
+    'user_id' => 1
+] ); 
+    
+header('location: /notes');
+die;
+  
+
+
