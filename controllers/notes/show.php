07a4979c8400ab1faeadbb246f5aeed04ce8b742
sytnic <sytnic@list.ru>
2023-10-17 16:01:56 +0300

    34.0 Destroy controller

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -8,25 +8,6 @@ $db = new Database($config['database']);
 $heading = "Note";
 $currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-  // delete action 
-
-  $note = $db->query('select * from notes where id = :id', [
-      'id' => $_GET['id']
-  ])->findOrFail();
-
-  authorize($note['user_id'] === $currentUserId);
-
-  $db->query('delete from notes where id = :id', [
-      'id' => $_GET['id']
-]);
-
-  header('location: /notes');
-  exit();
-
-} else {
-
   // show action
 
   $note = $db->query('select * from notes where id = :id', [
@@ -42,7 +23,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       'heading' => $heading,
       'note' => $note
     ]);
-
-}
 
 ?>
