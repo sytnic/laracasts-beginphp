8b8679038e62f28f0447119786cea2bc572bc889
sytnic <sytnic@list.ru>
2023-10-24 16:38:28 +0300

    36.1 Added button Edit

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -9,6 +9,10 @@
       </p> 
       <p> <?= htmlspecialchars($note['body']) ?> </p>      
     
+    <footer class="mt-6">
+      <a href="/note/edit" class="text-gray-500 border border-current px-3 py-1 rounded">Edit</a>
+    </footer>
+
     <form class="mt-6" method="POST">
       <input type="hidden" name="_method" value="DELETE">
       <input type="hidden" name="id" value="<?= $note['id'] ?>">
