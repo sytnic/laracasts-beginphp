2179ca1182ffa9371781969e78b2acd8fd436a50
sytnic <sytnic@list.ru>
2023-10-30 16:39:15 +0300

    38.1 Form Inputs Validation

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,3 +1,35 @@
 <?php
 
-dd($_POST);
+//dd($_POST);
+
+use Core\Validator;
+//use Core\App;
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// validate the form inputs
+$errors = [];
+
+if (! Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (! Validator::string($password, 7, 255)) {
+    $errors['password'] = 'Please provide a password of at least 7 characters.';
+}
+
+//dd($errors);
+
+if (! empty($errors)) {
+    return view("registration/create.view.php", [
+        'errors' => $errors
+    ]);
+}
+
+//$db = App::resolve(Database::class);
+
+// check if the account already exists
+    // if yes, redirect to a login page.
+    // if not, cave one to the database, and then log the user in, and redirect.
