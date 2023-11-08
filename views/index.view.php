033f888301acb9db1e8042385c39a15e0e2ad33e
sytnic <sytnic@list.ru>
2023-11-08 15:42:13 +0300

    40. Password hash

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p>Hello. Welcome to the Home page.</p>
+      <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the Home page.</p>
     </div>
   </main>
 
