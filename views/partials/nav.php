7bf218dffe5d6e08e85f7199736002a5faa936d3
sytnic <sytnic@list.ru>
2023-11-14 16:04:33 +0300

    41.6 Link Notes is hidden for an unauthorized user

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -13,10 +13,10 @@
               
               <a href="/about"    class="<?= urlIs("/about") ? "bg-gray-900 text-white " : "text-gray-300 " ?>
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">About</a>
-
+              <?php if($_SESSION['user'] ?? false) : ?>
               <a href="/notes"    class="<?= urlIs("/notes") ? "bg-gray-900 text-white " : "text-gray-300 " ?>              
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Notes</a>
-              
+              <?php endif; ?>
               <a href="/contact"  class="<?= urlIs("/contact") ? "bg-gray-900 text-white " : "text-gray-300 " ?>
                                             hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">Contact</a>
               
